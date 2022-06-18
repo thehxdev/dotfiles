12ca854f68e41f349962e0c73db1156df2561466
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-18 17:39:36 +0430

    Add files via upload

---
+++ kitty/README.md
@@ -0,0 +1,4 @@
+# kitty-dracula
+Kitty terminal with dracula colortheme and source code pro font.
+
+- Put all files in $HOME/.config/kitty/ directory.
---
+++ qtile-install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo pacman -Sy --needed --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome bleachbit
+
+# yay -S nerd-fonts-mononoki ttf-mononoki pfetch
+
+# pip install psutil
