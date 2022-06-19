aa4b21646bb53785dfb609754c0405ba626610eb
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-19 21:58:22 +0430

    Update qtile-install.sh

--- qtile-install.sh
+++ qtile-install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip maim
+sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip maim polkit lxsession
 # yay -S flameshot
 # yay -S nerd-fonts-mononoki ttf-mononoki pfetch
 # yay -S nerd-fonts-complete
