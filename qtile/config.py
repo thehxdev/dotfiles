d2dd1f8bf321c7729c9d9fc1cbf5b03b9d9cfb24
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-19 13:29:54 +0430

    Update config.py

--- qtile/config.py
+++ qtile/config.py
@@ -60,6 +60,8 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+	
+    Key([mod], "f", lazy.window.toggle_floating()),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
