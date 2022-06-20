23cd9e367ad8554cdc562e7a3356d01dc0c2bd2b
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-20 11:39:14 +0430

    Update qtile-install.sh

--- qtile-install.sh
+++ qtile-install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip maim polkit lxsession xdg-user-dirs
-# yay -S flameshot
+sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip polkit lxsession xdg-user-dirs feh
+# yay -S flameshot xcursor-breeze
 # yay -S nerd-fonts-mononoki ttf-mononoki pfetch
 # yay -S nerd-fonts-complete
 # pip install psutil
