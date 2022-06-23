aae3c37f47f8a5fa5e9a2d45945fdb7f5de9b2e2
Hx <93550458+hxdevlover@users.noreply.github.com>
2022-06-23 11:53:25 +0430

    Update qtile-install.sh

--- qtile-install.sh
+++ qtile-install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip polkit lxsession xdg-user-dirs feh
-# yay -S flameshot xcursor-breeze
-# yay -S nerd-fonts-mononoki ttf-mononoki pfetch
+sudo pacman -Sy --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings qtile nitrogen picom pcmanfm lxappearance materia-gtk-theme papirus-icon-theme archlinux-wallpapre arc-gtk-theme rofi alacritty kitty volumeicon networkmanager-applet alsa-utils unrar unzip p7zip xarchiver openvpn networkmanater-openvpn python-pip bashtop htop geany vim xreader ttf-roboto ttf-roboto-mono ttf-font-awesome ttf-ubuntu-font-family nerd-fonts-fira-code ttf-fira-code bleachbit gvfs ntfs-3g vi vim nano galculator brightnessctl font-manager sxiv yt-dlp xorg-xclipboard xclip polkit lxsession xdg-user-dirs feh acpid neovim jedi-language-server curl wget go ripgrep fd python-pip 
+# yay -S flameshot xcursor-breeze nerd-fonts-mononoki ttf-mononoki pfetch samim-fonts
 # yay -S nerd-fonts-complete
 # pip install psutil
