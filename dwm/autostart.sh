b034f82540de687b8b44d482b3842d1d63d4fc1f
hxdevlover <xclimax1324@gmail.com>
2022-08-24 20:55:18 +0430

    updated

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,13 +1,28 @@
 #! /usr/bin/env bash
 
-killall volumeicon
+#killall volumeicon &
+killall -q pulseaudio 
 
-pulseaudio --start 
 $HOME/.dwm/bar/dwm_bar.sh &
 nitrogen --restore &
 xrdb ~/.Xresources &
-nm-applet &
-/run/current-system/sw/bin/emacs --daemon &
-flameshot &
-sleep 1; exec volumeicon &
+#nm-applet &
+picom --experimental-backend &
+xfce4-power-manager &
+setxkbmap -layout "us,ir" -option "caps:escape,grp:alt_shift_toggle" &
+xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1 &
+/usr/bin/emacs --daemon &
+thunar --daemon &
+sleep 1; pulseaudio --start &
+#sleep 1; exec volumeicon &
+
+# exports
+export TERMINFO=/usr/share/terminfo/ &
+export PATH="/opt/sublime_text/:$PATH" &
+export PATH="$HOME/.cargo/bin/:$PATH" &
+export PATH="$HOME/.anaconda3/bin/:$PATH" &
+export PATH="$HOME/.emacs.d/bin/:$PATH" &
+export PATH="$HOME/.ghcup/bin/:$PATH" &
+export PATH="$HOME/.zls/:$PATH" &
+export PATH="$HOME/.local/bin/:$PATH" &
 
