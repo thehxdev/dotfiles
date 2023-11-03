2bf2908fb83163bc1ac8334fc0805c78b4080aa2
thehxdev <xclimax1324@gmail.com>
2023-11-03 10:42:58 +0330

    updated

--- WM/bspwm/autostart.sh
+++ WM/bspwm/autostart.sh
@@ -1,18 +1,18 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-#function run {
-#  if ! pgrep $1 ;
-#  then
-#    $@&
-#  fi
-#}
-
-#Find out your monitor name with xrandr or arandr (save and you get this line)
-#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
-#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
-#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-#autorandr horizontal
-
-$HOME/.config/polybar/launch.sh &
-
+nitrogen --restore &
+#picom --backend glx &
+nm-applet &
+#xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1 &
+#setxkbmap -layout 'us,ir' -option 'grp:alt_shift_toggle' &
+xset r rate 220 30 &
+xfce4-power-manager &
+flameshot &
+#xrdb /home/hx/.Xresources
+if ! pgrep clipman >/dev/null 2>&1; then
+    xfce4-clipman &
+fi
+#blueman-applet &
+#blueberry-tray &
+#volumeicon &
+#lxsession &
