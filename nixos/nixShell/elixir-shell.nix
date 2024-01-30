7bc50c9060e84271e2f0d377f7cb932eb7121c20
thehxdev <xclimax1324@gmail.com>
2024-01-30 15:59:54 +0330

    removed unused configs

--- nixos/nixShell/elixir-shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [
-        elixir_1_15
-        elixir-ls
-        rnix-lsp
-    ];
-}
