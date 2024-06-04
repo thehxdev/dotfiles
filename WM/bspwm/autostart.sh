760a3d19c25643bf2bb7481bcf57a0e8858c0aa3
thehxdev <hossein.khosravi.ce@gmail.com>
2024-06-04 15:15:01 +0330

    updated configs

--- WM/bspwm/autostart.sh
+++ WM/bspwm/autostart.sh
@@ -4,7 +4,7 @@ nitrogen --restore &
 #picom --backend glx &
 nm-applet &
 #xinput set-prop "ETPS/2 Elantech Touchpad" "libinput Tapping Enabled" 1 &
-#setxkbmap -layout 'us,ir' -option 'grp:alt_shift_toggle' &
+setxkbmap -layout 'us,ir' -option 'eurosign:e,caps:escape,grp:alt_shift_toggle' &
 xset r rate 220 30 &
 xfce4-power-manager &
 flameshot &
