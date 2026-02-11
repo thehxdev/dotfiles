b4e021aed793f69d5720cc05d5f38a637fd78775
thehxdev <hossein.khosravi.ce@gmail.com>
2026-02-11 17:59:37 +0330

    updated powershell profile script
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,15 @@
+# Import the module
+Import-Module PSFzf
+
+# Replace 'Ctrl+R' with a fuzzy history search
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
 function devshell {
-  pwsh.exe -NoExit -Command '&"D:\VS\Microsoft Visual Studio\18\BuildTools\Common7\Tools\Launch-VsDevShell.ps1" -Arch arm64 -HostArch amd64 -SkipAutomaticLocation'
+  pwsh.exe -NoExit -Command '&"D:\VS\Microsoft Visual Studio\18\BuildTools\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64 -HostArch amd64 -SkipAutomaticLocation'
 }
 
-function eza-alias {
-  eza -lh --group-directories-first
+function eza-alias() {
+  eza -lh --group-directories-first $args
 }
 
 Set-Alias -Name ls -Value eza-alias
